bdab545ed01d8e715de46b136a0af03cfb7b123a
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-04-18 23:00:50 -0300

    chore: Day 02

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { Header, Player } from '../components';
+
+import styles from '../styles/app.module.scss';
+
+import '../styles/global.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
+}
+
+export default MyApp;
